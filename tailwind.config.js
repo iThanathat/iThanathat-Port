c3ba78235b8e1d99b6af505b29e8f200aa0ef9d5
iThanathat <ithanathat.brain@gmail.com>
2022-04-11 15:51:02 +0700

    Initialized project

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      height: {
+        'fit-content': 'fit-content',
+        '420px': '26.25rem'
+      },
+      width: {
+        'fit-content': 'fit-content'
+      },
+      maxHeight: {
+        '420px': '26.25rem'
+      },
+      minHeight: {
+        '420px': '26.25rem'
+      },
+      minWidth: {
+        '80': '20rem'
+      }
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["night"]
+  }
+}

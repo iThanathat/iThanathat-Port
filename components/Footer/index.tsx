7e92b5ddf51b9b053d73bd78c700559d88bb3632
iThanathat <ithanathat.brain@gmail.com>
2022-04-21 23:36:29 +0700

    added opacity for footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,6 +1,6 @@
 const Footer: React.FC = () => {
   return (
-    <div className="p-4 text-center">
+    <div className="p-4 text-center bg-base-100 opacity-25">
       &copy; {new Date().getFullYear()} Thanathat Surakhup. All Rights Reserved.
     </div>
   );

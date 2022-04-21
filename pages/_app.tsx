0d7d28e73ed5f4fdafcedb64a17b49aaf9e4986a
iThanathat <ithanathat.brain@gmail.com>
2022-04-21 23:39:49 +0700

    added AnimatePresense component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,19 @@ import '../styles/globals.css';
 import type {AppProps} from 'next/app';
 import {config} from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
+import {AnimatePresence} from 'framer-motion';
+import Layout from '../components/Layouts/main';
 
 config.autoAddCss = false;
 
-function MyApp({Component, pageProps}: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({Component, pageProps, router}: AppProps) {
+  return (
+    <AnimatePresence exitBeforeEnter initial={true}>
+      <Layout router={router}>
+        <Component {...pageProps} />
+      </Layout>
+    </AnimatePresence>
+  );
 }
 
 export default MyApp;

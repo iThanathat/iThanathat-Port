1283e493595a4ae957faf8f1e392048f929ce28c
iThanathat <ithanathat.brain@gmail.com>
2022-04-24 01:45:25 +0700

    added experiences/billme page

---
+++ pages/experiences/billme.tsx
@@ -0,0 +1,88 @@
+import {NextPage} from 'next';
+import Layout from '../../components/Layouts/page';
+import {motion} from 'framer-motion';
+import Image from 'next/image';
+import {
+  faArrowLeft,
+  faArrowUpRightFromSquare,
+} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import Badge from '../../components/Badge';
+import {List, ListItem} from '../../components/List';
+import Link from 'next/link';
+
+const Billme: NextPage = () => {
+  return (
+    <Layout>
+      <motion.div
+        initial="initial"
+        animate="animate"
+        exit={{opacity: 0}}
+        className="mt-12"
+      >
+        <div className="h-screen">
+          <div className="container p-2 m-auto text-center">
+            <Link href="/experiences">
+              <div className="relative w-fit-content top-14 left-1/4 cursor-pointer">
+                <FontAwesomeIcon icon={faArrowLeft} size="2x" />
+                <p>Back</p>
+              </div>
+            </Link>
+            <h1 className="text-2xl">Billme Landing Page</h1>
+            <p className="mt-4">
+              This is a landing page for advertising
+              <Link href="https://billbox.billme.co.th">
+                <span className="ml-1 bg-primary text-primary-content cursor-pointer px-1 rounded">
+                  BillBox
+                </span>
+              </Link>{' '}
+              application.
+            </p>
+            <List className="mt-4 m-auto w-fit-content">
+              <ListItem className="flex">
+                <Badge>WEBSITE</Badge>
+                <Link href="https://billme.co.th" passHref>
+                  <a target="_blank">
+                    <p className="ml-4 hover:underline cursor-pointer">
+                      https://billme.co.th{' '}
+                      <FontAwesomeIcon
+                        icon={faArrowUpRightFromSquare}
+                        size="1x"
+                      />
+                    </p>
+                  </a>
+                </Link>
+              </ListItem>
+              <ListItem className="flex">
+                <Badge>STACK</Badge>
+                <p className="ml-4 ">React, Typescript</p>
+              </ListItem>
+            </List>
+            <div className="mt-4 flex flex-col w-fit-content m-auto">
+              <div>
+                <Image
+                  src="/images/billme-website.png"
+                  alt="Billme website"
+                  width={600}
+                  height={350}
+                  className="rounded-lg"
+                />
+              </div>
+              <div className="mt-4">
+                <Image
+                  src="/images/billme-website-2.png"
+                  alt="Billme website"
+                  width={600}
+                  height={300}
+                  className="rounded-lg"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </motion.div>
+    </Layout>
+  );
+};
+
+export default Billme;

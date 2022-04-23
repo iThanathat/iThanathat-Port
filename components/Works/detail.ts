8f06b312144cef9d25e1022733631863bf825b68
iThanathat <ithanathat.brain@gmail.com>
2022-04-24 01:45:58 +0700

    added router and href for detail

--- components/Works/detail.ts
+++ components/Works/detail.ts
@@ -1,17 +1,20 @@
 export const WorksDetail = [
   {
+    href: '/experiences/billme',
     imageSrc: '/images/billme-website.png',
     alt: 'billme-website',
     title: 'Billme Landing Page',
     description: '',
   },
   {
+    href: '/experiences/billbox',
     imageSrc: '/images/billbox-website.png',
     alt: 'billbox-website',
     title: 'BillBox',
     description: '',
   },
   {
+    href: '/experiences/billpouch',
     imageSrc: '/images/billpouch-website.png',
     alt: 'billpouch-website',
     title: 'BillPouch',
--- components/Works/index.tsx
+++ components/Works/index.tsx
@@ -2,17 +2,21 @@ import Image from 'next/image';
 import {motion} from 'framer-motion';
 import {fadeInUp} from './animate';
 import {WorksDetail} from './detail';
+import {useRouter} from 'next/router';
 
 const Works = () => {
+  const router = useRouter();
+
   return (
     <div className="h-screen">
-      <div className="pt-4 placeholder:h-fit-content grid grid-cols-2 w-full m-auto">
+      <div className="pt-4 h-fit-content grid grid-cols-2 w-full m-auto">
         {WorksDetail.map(detail => {
           return (
             <motion.div
               key={`detail-${detail.alt}`}
               variants={fadeInUp}
-              className="p-2 h-fit-content text-center"
+              className="p-2 h-fit-content text-center cursor-pointer"
+              onClick={() => router.push(`${detail.href}`)}
             >
               <Image
                 src={detail.imageSrc}

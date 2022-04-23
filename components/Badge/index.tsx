17473f22150a58e4077d6498411873bcd1c4ff75
iThanathat <ithanathat.brain@gmail.com>
2022-04-24 01:40:40 +0700

    added Badge component

---
+++ components/Badge/index.tsx
@@ -0,0 +1,13 @@
+type BadgeProps = {
+  children: React.ReactNode;
+};
+
+const Badge: React.FC<BadgeProps> = ({children}) => {
+  return (
+    <div className="px-1 flex flex-col justify-center rounded w-fit-content bg-primary text-primary-content text-sm">
+      {children}
+    </div>
+  );
+};
+
+export default Badge;

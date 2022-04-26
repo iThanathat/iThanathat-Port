288f79a3cd82e89c55c7927cff125818fe427d57
iThanathat <ithanathat.brain@gmail.com>
2022-04-27 01:13:49 +0700

    removed commend for unuse import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import {NextPage} from 'next';
 import {motion} from 'framer-motion';
 import Introduction from '../components/Introduction';
-// import Profile from '../components/Profile';
 import Layout from '../components/Layouts/page';
 
 const Home: NextPage = () => {
@@ -11,10 +10,8 @@ const Home: NextPage = () => {
         initial="initial"
         animate="animate"
         exit={{opacity: 0}}
-        // className="mt-12"
       >
         <Introduction />
-        {/* <Profile /> */}
       </motion.div>
     </Layout>
   );

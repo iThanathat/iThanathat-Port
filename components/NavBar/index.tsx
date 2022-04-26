e79cf2a9e65ef11c820601d17dfaf503a0ef0f7d
iThanathat <ithanathat.brain@gmail.com>
2022-04-27 01:13:05 +0700

    updated navbar visual

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -19,7 +19,7 @@ type NavLinkProps = {
 const NavLink: React.FC<NavLinkProps> = ({href, path, children}) => {
   const active = path === href;
   return (
-    <div className={`${active ? 'active' : 'inactive'}`}>
+    <div className={`h-full flex items-center ${active ? 'active' : 'inactive'}`}>
       <Link href={href} passHref>
         {children}
       </Link>
@@ -30,9 +30,9 @@ const NavLink: React.FC<NavLinkProps> = ({href, path, children}) => {
 const NavBar: React.FC<NavBarProps> = ({onToggleTheme, theme, path}) => {
   return (
     <div
-      className={'w-full bg-base-100 bg-opacity-75 backdrop-blur-sm fixed z-10'}
+      className={'w-full bg-base-100 bg-opacity-75 backdrop-blur-sm fixed z-10 h-12'}
     >
-      <div className="flex justify-center items-center p-2">
+      <div className="flex justify-between items-center h-full">
         <Link href="/" passHref>
           <div className="flex items-center">
             <>
@@ -41,62 +41,55 @@ const NavBar: React.FC<NavBarProps> = ({onToggleTheme, theme, path}) => {
                 alt="iThanathat"
                 width={30}
                 height={30}
-                className="cursor-pointer"
+                className="cursor-pointer rounded-full bg-primary"
               />
               <p
-                className={`text-xl ${
+                className={`text-xl ml-2 ${
                   theme === 'dark' ? 'text-white' : 'text-black'
                 } cursor-pointer`}
               >
-                Thanathat Surakhup
+                iThanathat
               </p>
             </>
           </div>
         </Link>
-        <div className="ml-4">
-          <NavLink path={path} href="/experiences">
-            <p
-              className={`text-xl ${
-                theme === 'dark' ? 'text-white' : 'text-black'
-              } cursor-pointer hover:underline mx-2`}
+        <div className="flex h-full items-center justify-evenly w-80">
+          <div className="h-full">
+            <NavLink path={path} href="/experiences">
+              <p
+                className={`text-xl ${
+                  theme === 'dark' ? 'text-white' : 'text-black'
+                } cursor-pointer mx-2`}
+              >
+                Experiences
+              </p>
+            </NavLink>
+          </div>
+          <div>
+            <Link
+              href={'https://github.com/iThanathat/iThanathat-Space'}
+              passHref
             >
-              Experiences
-            </p>
-          </NavLink>
-          {/* <Link href={'/experiences'} passHref>
-            <p
-              className={`text-xl ${
-                theme === 'dark' ? 'text-white' : 'text-black'
-              } cursor-pointer hover:underline`}
-            >
-              Experiences
-            </p>
-          </Link> */}
-        </div>
-        <div className="ml-6">
-          <Link
-            href={'https://github.com/iThanathat/iThanathat-Space'}
-            passHref
-          >
-            <a target="_blank">
-              <div className="flex items-center">
-                <FontAwesomeIcon
-                  icon={faGithub}
-                  size="2x"
-                  className={`text-primary-content ${
-                    theme === 'dark' ? 'text-white' : 'text-black'
-                  }`}
-                />
-                <p
-                  className={`ml-2 text-xl ${
-                    theme === 'dark' ? 'text-white' : 'text-black'
-                  } cursor-pointer hover:underline`}
-                >
-                  Source
-                </p>
-              </div>
-            </a>
-          </Link>
+              <a target="_blank">
+                <div className="flex items-center">
+                  <FontAwesomeIcon
+                    icon={faGithub}
+                    size="2x"
+                    className={`text-primary-content ${
+                      theme === 'dark' ? 'text-white' : 'text-black'
+                    }`}
+                  />
+                  <p
+                    className={`ml-2 text-xl ${
+                      theme === 'dark' ? 'text-white' : 'text-black'
+                    } cursor-pointer hover:underline`}
+                  >
+                    Source Code
+                  </p>
+                </div>
+              </a>
+            </Link>
+          </div>
         </div>
         <div onClick={() => onToggleTheme()} className="ml-6 cursor-pointer">
           <FontAwesomeIcon

5591ad4c7e9836ca1b6ec50ff9c57af5dbd986b9
iThanathat <ithanathat.brain@gmail.com>
2022-04-27 01:13:29 +0700

    moved variant value in component

--- components/Works/index.tsx
+++ components/Works/index.tsx
@@ -1,31 +1,47 @@
 import Image from 'next/image';
 import {motion} from 'framer-motion';
-import {fadeInUp} from './animate';
 import {WorksDetail} from './detail';
 import {useRouter} from 'next/router';
 
 const Works = () => {
   const router = useRouter();
+  const variants = {
+    initial: {
+      opacity: 0,
+      y: 60,
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.5,
+        ease: 'easeInOut',
+      },
+    },
+  }
 
   return (
     <div className="h-screen">
-      <div className="pt-4 h-fit-content grid grid-cols-2 w-full m-auto">
+      <div className="pt-4 h-full w-full m-auto grid grid-cols-1">
         {WorksDetail.map(detail => {
           return (
             <motion.div
               key={`detail-${detail.alt}`}
-              variants={fadeInUp}
-              className="p-2 h-fit-content text-center cursor-pointer"
+              variants={variants}
+              className="p-2 h-fit-content w-fit text-center cursor-pointer mx-auto"
               onClick={() => router.push(`${detail.href}`)}
             >
               <Image
                 src={detail.imageSrc}
                 alt={detail.alt}
-                width={450}
-                height={300}
+                width={400}
+                height={250}
                 className="rounded-lg"
               />
-              <h1>{detail.title}</h1>
+              <div className="mt-4">
+                <h1 className="text-xl">{detail.title}</h1>
+                <p>{detail.description}</p>
+              </div>
             </motion.div>
           );
         })}

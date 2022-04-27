582c43eb966e49110852041c04f3965534066832
iThanathat <ithanathat.brain@gmail.com>
2022-04-27 22:28:02 +0700

    added description for each app

--- components/Works/detail.ts
+++ components/Works/detail.ts
@@ -4,20 +4,20 @@ export const WorksDetail = [
     imageSrc: '/images/billme-website.png',
     alt: 'billme-website',
     title: 'Billme Landing Page',
-    description: '',
+    description: 'Advertisement landing page for Billme',
   },
   {
     href: '/experiences/billbox',
     imageSrc: '/images/billbox-website-2.png',
     alt: 'billbox-website',
     title: 'BillBox',
-    description: '',
+    description: 'An application for generate and send financial document',
   },
   {
     href: '/experiences/billpouch',
     imageSrc: '/images/billpouch-website.png',
     alt: 'billpouch-website',
     title: 'BillPouch',
-    description: '',
+    description: 'An application for receiving an invoice created by BillBox',
   },
 ];
